31e176edf5d9f636eff827ad884a4f9b44693e54
Marcelo Moraes <112552958+marcelomoraes1978@users.noreply.github.com>
2024-01-27 19:56:01 -0300

    meu site
    esse é meu site, vou melhorando aos poucos!!

---
+++ script.js
@@ -0,0 +1,4 @@
+// Exemplo de JavaScript (pode ser expandido conforme necessário)
+document.addEventListener("DOMContentLoaded", function () {
+    // Adicione aqui qualquer script JavaScript que você queira executar após o carregamento da página
+});
